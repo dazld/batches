1e2b3f0a66be9271456e9e176329650d83c81d36
Dan Peddle <dan@flarework.com>
2018-10-14 17:12:43 +0200

    pass or process values..?

--- src/batches/core.clj
+++ src/batches/core.clj
@@ -7,25 +7,29 @@
   (stop [this]))
 
 (defn accumulate
-  "Periodically invoke action with values, which can be conj'd onto the store with add. Allows the invocation to be cancelled´´
-  by calling (stop (accumulate identity))"
+  "Periodically invoke action with values, which can be pushed into the queue with (add this value).
+  Allows the invocation to be cancelled by calling (stop (accumulate identity))"
   [action time-ms]
   (let [stop (a/chan 1)
         push (a/chan)
         timer (a/chan)
-        _    (a/go-loop []
-               (a/<! (a/timeout time-ms))
-               (a/>! timer :go)
-               (recur))]
-    (a/go-loop [vals []]
-      (let [[v ch] (a/alts! [push timer stop] :priority true)]
-        (condp = ch
-          push (recur (conj vals v))
-          timer (do
-                  (action vals)
-                  (recur []))
-          stop vals)))
+        _ (a/go-loop []
+            (a/<! (a/timeout time-ms))
+            (a/>! timer :go)
+            (recur))
+        main-loop (a/go-loop [vals []]
+                    (let [[v ch] (a/alts! [push timer stop] :priority true)]
+                      (condp = ch
+                        push (recur (conj vals v))
+                        timer (do
+                                (action vals)
+                                (recur []))
+                        stop (do
+                               (action vals)
+                               vals))))]
     (reify
       Accumulating
-        (add [_ v] (a/put! push v))
-        (stop [_] (a/put! stop :stop)))))
+      (add [_ v] (a/put! push v))
+      (stop [_]
+        (a/put! stop :stop)
+        (a/<!! main-loop)))))
--- test/batches/core_test.clj
+++ test/batches/core_test.clj
@@ -4,25 +4,28 @@
 
 
 (deftest multithreaded
-  (let [futures 500
+  (let [futures 1000
+        wait 200
         result (atom [])
-        foo (bc/accumulate (fn [v]
-                             (swap! result
-                                    (fn [results]
-                                      (conj results (reduce + 0 v)))))
-                           100)
+        action (fn [v]
+                 (swap! result
+                        (fn [results]
+                          (conj results (reduce + 0 v)))))
+        foo (bc/accumulate action
+                           wait)
         worker (fn []
                  (future
                    (bc/add foo 1)))]
     (dotimes [_ futures]
       (worker))
-    (Thread/sleep 100)
+    (Thread/sleep wait)
     (is (= (first @result) futures))
     (dotimes [_ futures]
       (worker))
-    (Thread/sleep 100)
+    (Thread/sleep wait)
     (is (= (first @result) futures))
-    (bc/stop foo)))
+    (bc/add foo 1)
+    (is (= (bc/stop foo) [1]))))
 
 
 (deftest booms
