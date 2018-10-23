5b44a8a6e9ef5d4649f013d0b85d19b7abadac27
Dan Peddle <dan@iamfy.co>
2018-10-23 14:34:18 +0200

    0.1.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject org.clojars.dazld/batches "0.1.0-SNAPSHOT"
-  :description "Accumulate values and periodically act on them"
+(defproject org.clojars.dazld/batches "0.1.0"
+  :description "Accumulate values from an input channel and periodically push them to a core async channel"
   :url "https://github.com/dazld/batches"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
