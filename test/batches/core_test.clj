70f617213df8ba1c93daf166fd9b4de066fd37b1
Dan Peddle <dan@iamfy.co>
2018-10-23 14:26:54 +0200

    update docs

--- test/batches/core_test.clj
+++ test/batches/core_test.clj
@@ -9,27 +9,28 @@
   (testing "pushing many values"
     (let [in (a/chan 2000)
           out (a/chan)
-          foo (bc/accumulate 150 in out)]
+          foo (bc/accumulate 600 in out)
+          then (System/currentTimeMillis)]
 
       (doseq [n (range 10000)]
         (a/>!! in 1))
 
-      (let [then (System/currentTimeMillis)
-            result (reduce + 0 (a/<!! out))
+      (let [result (reduce + 0 (a/<!! out))
             elapsed (- (System/currentTimeMillis) then)]
-        (is (> elapsed 100))
+        (println "Expected around 600ms, took:" (str elapsed "ms"))
+        (is (> elapsed 500))
         (is (= result 10000))
         (is (= [] (a/<!! out))))))
 
   (testing "pushing `:stop` will stop recursion and drain any pending values directly into out"
     (let [in (a/chan)
           out (a/chan)
-          foo (bc/accumulate 5000 in out)]
+          foo (bc/accumulate 5000 in out)
+          then (System/currentTimeMillis)]
 
       (a/>!! in :foo)
       (a/>!! in :stop)
-      (let [then (System/currentTimeMillis)
-            result (a/<!! out)
+      (let [result (a/<!! out)
             elapsed (- (System/currentTimeMillis) then)]
         (is (= result [:foo]))
         (is (<= elapsed 1))))))
