b4d311b746f28ffad59bf7240f24a9c68024493f
Dan Peddle <dan@iamfy.co>
2018-10-23 12:00:36 +0200

    clojars ID

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject com.flarework/batches "0.1.0-SNAPSHOT"
-  :description "Accumulate values into an atom and periodically act on them"
-  :url "https://flarework.com"
+(defproject org.clojars.dazld/batches "0.1.0-SNAPSHOT"
+  :description "Accumulate values and periodically act on them"
+  :url "https://github.com/dazld/batches"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [tortue/spy "1.4.0"]
-                 [org.clojure/core.async "0.4.474"]]
-  :aliases {"rebl" ["trampoline" "run" "-m" "rebel-readline.main"]})
+                 [org.clojure/core.async "0.4.474"]])
