45dff6567c193d1ef3ad40f2297e5e423193849f
Dan Peddle <dan@iamfy.co>
2018-10-23 13:36:16 +0200

    check time has passed or not

--- test/batches/core_test.clj
+++ test/batches/core_test.clj
@@ -9,21 +9,28 @@
   (testing "pushing many values"
     (let [in (a/chan 2000)
           out (a/chan)
-          foo (bc/accumulate 10 in out)]
+          foo (bc/accumulate 150 in out)]
 
       (doseq [n (range 10000)]
         (a/>!! in 1))
 
-      (is (= 10000 (reduce + 0 (a/<!! out))))
-      (is (= [] (a/<!! out)))))
+      (let [then (System/currentTimeMillis)
+            result (reduce + 0 (a/<!! out))
+            elapsed (- (System/currentTimeMillis) then)]
+        (is (> elapsed 100))
+        (is (= result 10000))
+        (is (= [] (a/<!! out))))))
 
-  (testing "pushing `:stop` will stop recursion and drain any pending values"
+  (testing "pushing `:stop` will stop recursion and drain any pending values directly into out"
     (let [in (a/chan)
           out (a/chan)
           foo (bc/accumulate 5000 in out)]
 
       (a/>!! in :foo)
       (a/>!! in :stop)
-
-      (is (= [:foo] (a/<!! out))))))
+      (let [then (System/currentTimeMillis)
+            result (a/<!! out)
+            elapsed (- (System/currentTimeMillis) then)]
+        (is (= result [:foo]))
+        (is (<= elapsed 1))))))
 
